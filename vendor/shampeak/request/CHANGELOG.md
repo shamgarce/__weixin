89b63bceb3c65051dadabaf3e3d1709e7337506d
shampeak <976107540@qq.com>
2015-10-27 17:49:12 +0800

    0.0002

---
+++ vendor/shampeak/request/CHANGELOG.md
@@ -0,0 +1,5 @@
+#Changelog
+
+All Notable changes to `League\Url` will be documented in this file
+
+
---
+++ vendor/shampeak/request/src/Request.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * 调用 $req = Sham\Request::getInstance();
+ */
+namespace Sham;
+
+class Request extends Http\Request
+{
+}
